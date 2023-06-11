4ae2a5ba0a8fda5e84b1227d247619561c7dfd0a
Rithesh Kumar singh <99482880+rickx-x@users.noreply.github.com>
2023-06-11 20:05:03 +0530

    Update App.js
    made changes for display of true home page at first

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Shop />} />
+            <Route path="/Plantify/" element={<Shop />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
